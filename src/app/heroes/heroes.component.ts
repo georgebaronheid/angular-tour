9c50a86a71e4d67e16b12a24d1e340978cd8e953
George Baronheid <baronheid.george@gmail.com>
2020-10-24 18:57:38 -0300

    Displayed Heroes list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../Hero';
+import {HEROES} from '../../heroes-mock';
 
 @Component({
   selector: 'app-heroes',
@@ -7,14 +8,14 @@ import { Hero } from '../Hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-
-  hero: Hero = {
-    id: 1,
-    name: 'Capitão Arroz'
-  };
   constructor() { }
+
+  heroes = HEROES;
+  selectedHero: Hero;
 
   ngOnInit(): void {
   }
-
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
---
+++ src/heroes-mock.ts
@@ -0,0 +1,14 @@
+import {Hero} from './app/Hero';
+
+export const HEROES: Hero[] = [
+  {id: 11, name: 'América'},
+  {id: 12, name: 'Batata'},
+  {id: 13, name: 'Arroz'},
+  {id: 14, name: 'Luz'},
+  {id: 15, name: 'Escuridão'},
+  {id: 16, name: 'Paz'},
+  {id: 17, name: 'Guerra'},
+  {id: 18, name: 'Ares'},
+  {id: 19, name: 'Hades'},
+  {id: 20, name: 'Thor'}
+];
